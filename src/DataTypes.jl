c6f64814fa319f92cfde015808ad22ac1a60d1b7
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2018-01-30 16:43:37 +0100

    reimplement abstract interfaces

--- src/DataTypes.jl
+++ src/DataTypes.jl
@@ -1,10 +1,9 @@
 
 # depend explicitly on AxisArrays (sparse) in some implementation?
 
-abstract type AbstractThings{T, N, M <: AbstractArray{N, T}} end
-abstract type AbstractTimeThings{T, M} <: AbstractThings{T, 3, M} end
-abstract type AbstractOnceThings{T, M} <: AbstractThings{T, 2, M} end
+abstract type AbstractThings end
 abstract type AbstractPlaces end
-abstract type AbstractGrid <: AbstractSites end
-abstract type AbstractPoints <: AbstractSites end
-abstract type AbstractAssemblage{T <: AbstractThings, P <: AbstractPlaces} end
+abstract type AbstractGrid <: AbstractPlaces end
+abstract type AbstractPoints <: AbstractPlaces end
+abstract type AbstractAssemblage{T <: AbstractThings, P <: AbstractPlaces} <: AbstractTemporalAssemblage end
+abstract type AbstractTemporalAssemblage end 
--- src/Interface.jl
+++ src/Interface.jl
@@ -1,9 +1,9 @@
 
 
 # Functions
-occurrences(com::AbstractThings) = error("function not defined for this type") #has to be implemented with the concrete type
-thingnames(com::AbstractThings) = error("function not defined for this type")
-view(com::AbstractThings) = error("function not defined for this type")
+occurrences(com::AbstractAssemblage) = error("function not defined for this type") #has to be implemented with the concrete type
+thingnames(com::AbstractAssemblage) = error("function not defined for this type")
+view(com::AbstractAssemblage) = error("function not defined for this type")
 places(asm::AbstractAssemblage) = error("function not defined for this type")
 things(asm::AbstractAssemblage) = error("function not defined for this type")
 nplaces(asm::AbstractPlaces) = error("function not defined for this type")
@@ -12,10 +12,10 @@ placenames(com::AbstractPlaces) = error("function not defined for this type")
 nzrows(a::AbstractMatrix) = find(sum(a .> 0, 2)> 0)
 nzcols(a::AbstractMatrix) = find(sum(a .> 0, 1)> 0)
 
-occurring(com::AbstractThings) = nzrows(occurrences(com))
-occupied(com::AbstractThings) = nzcols(occurrences(com))
-occupied(com::AbstractOnceThings, idx) = findn(occurrences(com)[idx,:])
-occurring(com::AbstractOnceThings, idx) = findn(occurrences(com)[:,idx])
+occurring(com::AbstractAssemblage) = nzrows(occurrences(com))
+occupied(com::AbstractAssemblage) = nzcols(occurrences(com))
+occupied(com::AbstractAssemblage, idx) = findn(occurrences(com)[idx,:])
+occurring(com::AbstractAssemblage, idx) = findn(occurrences(com)[:,idx])
 
 noccurring(x) = length(occurring(x))
 noccupied(x) = length(occupied(x))
@@ -23,23 +23,21 @@ noccurring(x, idx) = length(occurring(x, idx))
 noccupied(x, idx) = length(occupied(x, idx))
 
 nthings(com::AbstractThings) = size(occurrences(com), 1)
-nplaces(com::AbstractThings) = size(occurrences(com), 2)
-ntimes(com::AbstractTimeThings) = size(occurrences(com), 3)
+nplaces(com::AbstractPlaces) = size(occurrences(com), 2)
 
-thingoccurrences(com::AbstractOnceThings, idx) = view(occurrences(com), idx, :)
-placeoccurrences(com::AbstractOnceThings, idx) = view(occurrences(com), :, idx) # make certain that the view implementation also takes thing or place names
+thingoccurrences(com::AbstractAssemblage, idx) = view(occurrences(com), idx, :)
+placeoccurrences(com::AbstractAssemblage, idx) = view(occurrences(com), :, idx) # make certain that the view implementation also takes thing or place names
 
+richness(com::AbstractAssemblage{Bool}) = vec(sum(occurrences(com), 1))
+richness(com::AbstractAssemblage{<:Real}) = vec(mapslices(nnz, occurrences(com), 1))
 
-richness(com::AbstractThings{Bool}) = vec(sum(occurrences(com), 1))
-richness(com::AbstractThings{<:Real}) = vec(mapslices(nnz, occurrences(com), 1))
+occupancy(com::AbstractAssemblage{Bool}) = vec(sum(occurrences(com), 2))
+occupancy(com::AbstractAssemblage{<:Real}) = vec(mapslices(nnz, occurrences(com), 2))
 
-occupancy(com::AbstractThings{Bool}) = vec(sum(occurrences(com), 2))
-occupancy(com::AbstractThings{<:Real}) = vec(mapslices(nnz, occurrences(com), 2))
+records(com::AbstractAssemblage) = nnz(occurrences(com))
 
-records(com::AbstractComMatrix) = nnz(occurrences(com))
-
-cooccurring(com::AbstractThings, inds...) = cooccurring(com, [inds...])
-function cooccurring(com::AbstractThings, inds::AbstractVector)
+cooccurring(com::AbstractAssemblage, inds...) = cooccurring(com, [inds...])
+function cooccurring(com::AbstractAssemblage, inds::AbstractVector)
     sub = view(com, species = inds)
     richness(sub) .== nthings(sub)
 end
@@ -51,11 +49,6 @@ function createsummaryline(vec::AbstractVector{T}) where T<:AbstractString
     linefunc(vec[1:3])*"..."*linefunc(vec[(end-1):end])
 end
 
-function show(io::IO, com::T) where T <: AbstractOnceThings
-    sp = createsummaryline(thingnames(com))
-    si = createsummaryline(placenames(com))
-    println(io, "$T with $(nthings(com)) things in $(nplaces(com)) places\n\nThing names:\n$(sp)\n\nPlace names:\n$(si)")
-end
 
 function show(io::IO, com::T) where T <:AbstractAssemblage
     sp = createsummaryline(thingnames(com))
@@ -74,7 +67,7 @@ macro forward_func(ex, fs)
 end
 
 @forward_func AbstractAssemblage.places nplaces, placenames
-@forward_func AbstractAssemblage.things nthings, occupancy, richness, records, occurring, occupied, thingnames
+@forward_func AbstractAssemblage.things nthings, thingnames
 
 TODO:
 # accessing cache
