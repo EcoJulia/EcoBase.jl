bc4b756a795c3d073e5802cf1302a24108e6058c
Richard Reeve <git@richardreeve.net>
2018-02-04 16:34:19 +0000

    Potential fix to type hierarchy and minor tidying.

--- src/DataTypes.jl
+++ src/DataTypes.jl
@@ -1,9 +1,11 @@
 
-# depend explicitly on AxisArrays (sparse) in some implementation?
 
 abstract type AbstractThings end
 abstract type AbstractPlaces end
-abstract type AbstractGrid <: AbstractPlaces end
-abstract type AbstractPoints <: AbstractPlaces end
-abstract type AbstractAssemblage{T <: AbstractThings, P <: AbstractPlaces} <: AbstractTemporalAssemblage end
-abstract type AbstractTemporalAssemblage end 
+abstract type AbstractLocations <: AbstractPlaces end
+abstract type AbstractGrid <: AbstractLocations end
+abstract type AbstractAssemblage{T <: AbstractThings,
+                                 P <: AbstractPlaces,
+                                 D <: Real}
+
+# depend explicitly on AxisArrays (and/or sparse) in some implementation?
--- src/Interface.jl
+++ src/Interface.jl
@@ -1,38 +1,36 @@
-
-
-# Functions
-occurrences(com::AbstractAssemblage) = error("function not defined for this type") #has to be implemented with the concrete type
-thingnames(com::AbstractAssemblage) = error("function not defined for this type")
+# Functions - most have to be implemented with the concrete type
+occurrences(com::AbstractAssemblage) = error("function not defined for this type") 
 view(com::AbstractAssemblage) = error("function not defined for this type")
 places(asm::AbstractAssemblage) = error("function not defined for this type")
 things(asm::AbstractAssemblage) = error("function not defined for this type")
-nplaces(asm::AbstractPlaces) = error("function not defined for this type")
+
+nplaces(com::AbstractPlaces) = size(occurrences(com), 2)
 placenames(com::AbstractPlaces) = error("function not defined for this type")
 
-nzrows(a::AbstractMatrix) = find(sum(a .> 0, 2)> 0)
-nzcols(a::AbstractMatrix) = find(sum(a .> 0, 1)> 0)
+nthings(com::AbstractThings) = size(occurrences(com), 1)
+thingnames(com::AbstractThings) = error("function not defined for this type")
+
+nzrows(a::AbstractMatrix) = find(sum(a .> 0, 2) > 0)
+nzcols(a::AbstractMatrix) = find(sum(a .> 0, 1) > 0)
 
 occurring(com::AbstractAssemblage) = nzrows(occurrences(com))
 occupied(com::AbstractAssemblage) = nzcols(occurrences(com))
-occupied(com::AbstractAssemblage, idx) = findn(occurrences(com)[idx,:])
-occurring(com::AbstractAssemblage, idx) = findn(occurrences(com)[:,idx])
+occupied(com::AbstractAssemblage, idx) = findn(occurrences(com)[idx, :])
+occurring(com::AbstractAssemblage, idx) = findn(occurrences(com)[:, idx])
 
 noccurring(x) = length(occurring(x))
 noccupied(x) = length(occupied(x))
 noccurring(x, idx) = length(occurring(x, idx))
 noccupied(x, idx) = length(occupied(x, idx))
 
-nthings(com::AbstractThings) = size(occurrences(com), 1)
-nplaces(com::AbstractPlaces) = size(occurrences(com), 2)
-
 thingoccurrences(com::AbstractAssemblage, idx) = view(occurrences(com), idx, :)
 placeoccurrences(com::AbstractAssemblage, idx) = view(occurrences(com), :, idx) # make certain that the view implementation also takes thing or place names
 
-richness(com::AbstractAssemblage{Bool}) = vec(sum(occurrences(com), 1))
-richness(com::AbstractAssemblage{<:Real}) = vec(mapslices(nnz, occurrences(com), 1))
+richness(com::AbstractAssemblage{_, _, Bool}) = vec(sum(occurrences(com), 1))
+richness(com::AbstractAssemblage) = vec(mapslices(nnz, occurrences(com), 1))
 
-occupancy(com::AbstractAssemblage{Bool}) = vec(sum(occurrences(com), 2))
-occupancy(com::AbstractAssemblage{<:Real}) = vec(mapslices(nnz, occurrences(com), 2))
+occupancy(com::AbstractAssemblage{_, _, Bool}) = vec(sum(occurrences(com), 2))
+occupancy(com::AbstractAssemblage) = vec(mapslices(nnz, occurrences(com), 2))
 
 records(com::AbstractAssemblage) = nnz(occurrences(com))
 
@@ -42,15 +40,15 @@ function cooccurring(com::AbstractAssemblage, inds::AbstractVector)
     richness(sub) .== nthings(sub)
 end
 
-function createsummaryline(vec::AbstractVector{T}) where T<:AbstractString
-    linefunc(vec) = mapreduce(x->x*", ", *, vec[1:(end-1)])*vec[end]
+function createsummaryline(vec::AbstractVector{T}) where T <: AbstractString
+    linefunc(vec) = mapreduce(x->x * ", ", *, vec[1:(end-1)]) * vec[end]
     length(vec) == 1 && return vec[1]
     length(vec) < 6 && return linefunc(vec)
-    linefunc(vec[1:3])*"..."*linefunc(vec[(end-1):end])
+    linefunc(vec[1:3]) * "..." * linefunc(vec[(end-1):end])
 end
 
 
-function show(io::IO, com::T) where T <:AbstractAssemblage
+function show(io::IO, com::T) where T <: AbstractAssemblage
     sp = createsummaryline(thingnames(com))
     si = createsummaryline(placenames(com))
     println(io, "$T with $(nthings(com)) things in $(nplaces(com)) places\n\nThing names:\n$(sp)\n\nPlace names:\n$(si)")
@@ -60,7 +58,7 @@ end
 macro forward_func(ex, fs)
     T, field = ex.args[1], ex.args[2].args[1]
     T = esc(T)
-    fs = isexpr(fs, :tuple) ? map(esc, fs.args) : [esc(fs)]
+    fs = Meta.isexpr(fs, :tuple) ? map(esc, fs.args) : [esc(fs)]
     :($([:($f(x::$T, args...) = (Base.@_inline_meta; $f($(field)(x), args...)))
         for f in fs]...);
     nothing)
