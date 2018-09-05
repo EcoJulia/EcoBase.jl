e4eea3002d25925bdb2f1b06c944e7a77d05efd0
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2018-09-05 22:21:29 +0200

    don't export anything

--- src/EcoBase.jl
+++ src/EcoBase.jl
@@ -4,12 +4,11 @@ import Base: show
 import RecipesBase
 
 include("DataTypes.jl")
-
 include("Interface.jl")
-export nthings, nplaces, occupancy, richness, records, placenames, thingnames
-export occurring, noccurring, occupied, noccupied, occurrences
-export indices, coordinates, xcells, ycells, cells, xmin, xmax, ymin, ymax
-export xrange, yrange, xcellsize, ycellsize, cellsize
+#export nthings, nplaces, occupancy, richness, records, placenames, thingnames
+#export occurring, noccurring, occupied, noccupied, occurrences
+#export indices, coordinates, xcells, ycells, cells, xmin, xmax, ymin, ymax
+#export xrange, yrange, xcellsize, ycellsize, cellsize
 
 include("PlotRecipes.jl")
 end # module
