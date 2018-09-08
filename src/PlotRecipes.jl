fa91c438168f535798657aa3775e385bd67e5fd0
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2018-09-08 14:46:38 +0200

    fix plotting

--- src/PlotRecipes.jl
+++ src/PlotRecipes.jl
@@ -1,7 +1,7 @@
 
 
 function convert_to_image(var::AbstractVector, grd::AbstractGrid)
-    x = Matrix{Float64}(reverse(cells(grd))...)
+    x = Matrix{Float64}(undef, reverse(cells(grd))...)
     fill!(x, NaN)
     xind, yind =  indices(grd, 1), indices(grd,2) #since matrices are drawn from upper left corner
     [x[yind[i], xind[i]] = val for (i, val) in enumerate(var)]
@@ -35,18 +35,13 @@ RecipesBase.@recipe function f(asm::AbstractAssemblage; occupied = true)
         var = [Float64(v) for v in var]
         (var[var.==0] .= NaN)
     end
-    var, asm.site
+    var, getcoords(places(asm))
 end
-
 
 RecipesBase.@recipe function f(var::AbstractVector, asm::AbstractAssemblage)
-    var, asm.site
-end
-
-RecipesBase.@recipe function f(var::Symbol, asm::AbstractAssemblage)
-    asm.site.sitestats[var], asm.site
+    var, getcoords(places(asm))
 end
 
 RecipesBase.@recipe function f(g::Function, asm::AbstractAssemblage)
-    g(asm), asm.site
+    g(asm), getcoords(places(asm))
 end
