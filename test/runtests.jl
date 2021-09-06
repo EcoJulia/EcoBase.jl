27bd194c854a8017829cb534b66130a990f49f77
Richard Reeve <git@richardreeve.net>
2021-09-06 15:04:26 +0100

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,32 @@
+using Test
 
 # Identify files in test/ that are testing matching files in src/
 #  - src/Source.jl will be matched by test/test_Source.jl
 
-filebase = map(file -> replace(file, r"(.*).jl$" => s"\1"),
-                filter(file -> occursin(r".*\.jl$", file),
-                       readdir("../src")))
-testbase = map(file -> replace(file, r"test_(.*).jl$" => s"\1"),
+filebase = String[]
+for (root, dirs, files) in walkdir("../src")
+    append!(filebase,
+            map(file -> replace(file, r"(.*).jl" => s"\1"),
+                filter(file -> occursin(r".*\.jl", file), files)))
+end
+
+testbase = map(file -> replace(file, r"test_(.*).jl" => s"\1"),
                 filter(str -> occursin(r"^test_.*\.jl$", str), readdir()))
 
-println()
-@info "Running tests for files:"
-for t in testbase
-    println("    = $t.jl")
-end
-println()
+@testset "EcoBase.jl" begin
+    println()
+    @info "Running tests for files:"
+    for t in testbase
+        println("    = $t.jl")
+    end
+    println()
 
-@testset "* Testing $t.jl" for t in testbase
-    include("test_$t.jl")
+    @info "Running tests..."
+    @testset for t in testbase
+        fn = "test_$t.jl"
+        println("    * Testing $t.jl ...")
+        include(fn)
+    end
 end
 
 # Identify tests with no matching file
@@ -27,35 +37,14 @@ if length(superfluous) > 0
     for f in superfluous
         println("    + $f.jl")
     end
-    println()
 end
 
 # Identify files with no matching test
-missing = filter(f -> f ∉ testbase, filebase)
-if length(missing) > 0
+notest = filter(f -> f ∉ testbase, filebase)
+if length(notest) > 0
     println()
     @info "Potentially missing tests:"
-    for f in missing
+    for f in notest
         println("    - $f.jl")
-    end
-    println()
-end
-
-# Identify files that are cross-validating results against other packages
-# test/pkg_Package.jl should validate results against the Package package
-
-pkgbase = map(file -> replace(file, r"pkg_(.*).jl$" => s"\1"),
-                   filter(str -> occursin(r"^pkg_.*\.jl$", str),
-                          readdir()))
-
-if length(pkgbase) > 0
-    @info "Running cross-validation against:"
-    for p in pkgbase
-        println("    = $p")
-    end
-    println()
-
-    @testset " * Validating against $p" for p in pkgbase
-        include("pkg_$p.jl")
     end
 end
