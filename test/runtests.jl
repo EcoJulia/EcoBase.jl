63c3c4e1b6d9ffe166f98b526b7cd2d7e09fb7a2
richardreeve <richard.reeve@glasgow.ac.uk>
2024-07-21 21:26:08 +0100

    Clear unnecessary testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -44,40 +44,6 @@ if rsmd == "FALSE"
         println()
     end
 
-    # Identify files in test/ that are testing matching files in ext/
-    #  - ext/SourceExt.jl will be matched by test/ext_SourceExt.jl
-    filebase = String[]
-    for (root, dirs, files) in walkdir("../ext")
-        append!(filebase,
-                map(file -> replace(file, r"(.*).jl" => s"\1"),
-                    filter(file -> occursin(r".*\.jl", file), files)))
-    end
-
-    extbase = map(file -> replace(file, r"ext_(.*).jl" => s"\1"),
-                  filter(str -> occursin(r"^ext_.*\.jl$", str), readdir()))
-
-    # Identify tests with no matching file
-    superfluous = filter(f -> f ∉ filebase, extbase)
-    if length(superfluous) > 0
-        println()
-        @info "Potentially superfluous extension tests:"
-        for f in superfluous
-            println("    + $f.jl")
-        end
-        println()
-    end
-
-    # Identify files with no matching test
-    notest = filter(f -> f ∉ extbase, filebase)
-    if length(notest) > 0
-        println()
-        @info "Potentially missing extension tests:"
-        for f in notest
-            println("    - $f.jl")
-        end
-        println()
-    end
-
     # Seed RNG to make tests reproducible
     Random.seed!(1234)
 
@@ -94,20 +60,6 @@ if rsmd == "FALSE"
         @testset for t in testbase
             fn = "test_$t.jl"
             println("    * Testing $t.jl ...")
-            include(fn)
-        end
-
-        println()
-        @info "Running tests for extensions:"
-        for t in extbase
-            println("    = $t.jl")
-        end
-        println()
-
-        @info "Running extension tests..."
-        @testset for t in extbase
-            fn = "ext_$t.jl"
-            println("    * Testing $t.jl extension...")
             include(fn)
         end
     end
